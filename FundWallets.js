8d0357952e4ab703b8ee8df6a9df7b90c1e2cf82
PumpVial <contact@pumpvial.com>
2025-05-27 23:06:54 +0200

    Init commit

---
+++ FundWallets.js
@@ -0,0 +1,50 @@
+import { Keypair, VersionedTransaction, Connection } from "@solana/web3.js";
+import bs58 from "bs58";
+import axios from "axios";
+
+/////////////////////////////// CONFIG //////////////////////////////////////
+const API_KEY = process.env.PUMPVIAL_API_KEY;
+const RPC_URL = ""; // Visit Helius.dev
+const connection = new Connection(RPC_URL, "confirmed");
+const FUNDER_PRIVATE_KEY = "Enter the funder's private key here";
+const funderKeypair = Keypair.fromSecretKey(bs58.decode(FUNDER_PRIVATE_KEY));
+
+const wallets = [
+  { publicKey: "NkrHERKzAetSxbrWenirWbrd9YZ3kiaLWeeniQ", amount: "0.33" },
+  { publicKey: "xbrWSxyfrKC8kiQfSxrWSxyfeni7EshZBvp6Qb", amount: "0.2" },
+  // ... up to 50
+];
+//////////////////////////////// END ////////////////////////////////////////
+
+const payload = {
+  funderPubKey: funderKeypair.publicKey,
+  wallets,
+};
+
+const fundWallets = async () => {
+  const URL = "https://pumpvial.com/api/fund-wallets";
+
+  try {
+    const request = await axios.post(URL, payload, {
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": API_KEY,
+      },
+    });
+
+    const { serializedTransaction } = request.data;
+    const serialized = Uint8Array.from(
+      Buffer.from(serializedTransaction, "base64")
+    );
+
+    const transaction = VersionedTransaction.deserialize(serialized);
+    transaction.sign([funderKeypair]);
+
+    const signature = await connection.sendTransaction(transaction);
+    console.log(`Success: https://solscan.io/tx/${signature}`);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
+
+fundWallets();
---
+++ README.md
@@ -0,0 +1,45 @@
+# Fund Solana Wallets
+
+Quickly fund up to 50 Solana wallets per transaction. Keep private keys secure in your own code, and sign transaction locally.
+
+---
+
+## Prerequisites
+
+- Get your free API key from https://pumpvial.com
+
+---
+
+## Install Dependencies
+
+`npm i @solana/web3.js bs58 axios`
+
+---
+
+#### Request
+
+##### [required] [object]
+
+`funderPubKey`: Public key object of the transaction funder.
+
+##### [required] [array]
+
+`wallets`: Array of wallet objects to be funded.
+`[{`
+&nbsp;&nbsp;&nbsp;&nbsp;`string: publicKey`: Fund recipient wallet address.
+&nbsp;&nbsp;&nbsp;&nbsp;`string: amount`: SOL amount to receive.
+`}]`
+
+---
+
+#### Response
+
+`string: serializedTransaction`: Unsigned base64 encoded transaction.
+
+---
+
+Need help? Hop in our Discord: https://discord.gg/WBmZss3jQq
+
+---
+
+**Tags:** solana, solana-wallet, solana-wallet-funding, solana-tokens, solana-sdk, solana-web3, solana-transactions, solana-api, pumpfun, pumpvial, crypto-wallet, crypto-funding, blockchain, blockchain-wallet, crypto, cryptocurrency, defi, decentralized-finance, solana-developer, solana-devtools, solana-nft, solana-program, solana-sdk-js, solana-dapp, solana-smart-contracts, solana-bots, solana-automation, solana-trading, solana-token-transfer, solana-sol, web3, web3js, crypto-development, crypto-api, solana-tools, solana-sol-transfer, solana-multisig, solana-transaction-builder, solana-wallet-security, solana-payer, solana-base64, bs58, solana-sdk-tools, solana-transaction-signing, solana-pumpfun, solana-funding-tool
